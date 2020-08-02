61dd149632c7186849cc690a6f2587eeefc15438
dkersten <dkersten1994@gmail.com>
2020-08-02 14:18:51 -0400

    refactored scorecard to replace inputs with divs for improved UX on mobile

--- src/components/ScorecardRow.js
+++ src/components/ScorecardRow.js
@@ -5,28 +5,33 @@ const ScorecardRow = (props) => {
     const index = props.num
 
     // passes hole num and par up to scorecard component
+    // const handleParChange = (e) => {
+    //     props.changeParFunc(e.target.value, index)
+    // }
+
     const handleParChange = (e) => {
-        props.changeParFunc(e.target.value, index)
+        props.changeParFunc(parseInt(e.target.textContent), index)
     }
 
     // passes hole num and score up to scorecard component
     const handleScoreChange = (e) => {
-        props.changeScoreFunc(e.target.value, index)
+        props.changeScoreFunc(parseInt(e.target.textContent), index)
     }
 
-    const handleInputPar = () => {
+    const handlePar = () => {
         props.toggleModalFunc("par", props.num)
     }
-    const handleInputScore = () => {
+    const handleScore = () => {
         props.toggleModalFunc("score", props.num)
     }
 
     return(
         <tr>
             <td>{props.num}</td>
-            <td><input value={props.pars[index]} onChange={handleParChange} type="number" placeholder="Par" onFocus={handleInputPar} onClick={handleInputPar} /></td>
             
-            <td><input value={props.scores[index]} onChange={handleScoreChange} type="number" placeholder="Score" onFocus={handleInputScore} onClick={handleInputScore} /></td>
+            <td className="user-data"><div onClick={handlePar} onChange={handleParChange}>{props.pars[index]}</div></td>
+
+            <td className="user-data"><div onClick={handleScore} onChange={handleScoreChange}>{props.scores[index]}</div></td>
         </tr>
     )
 }
